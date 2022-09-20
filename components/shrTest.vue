b6fdeaa3eae7ec8ec8120e8182a60e8ae2b98db9
Luca Stave <98452756+staveluca@users.noreply.github.com>
2022-09-20 18:20:34 +0200

    first commit

---
+++ components/shrTest.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>PROVA</h1>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+  <h1 class="text-red-600">HELLO</h1>
+  <shrTest />
+  <shrTest />
+  <shrTest />
+</div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
