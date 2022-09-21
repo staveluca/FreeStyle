9a7cef1f84f9e8e482a1517ea458cfbf832db616
Luca Stave <98452756+staveluca@users.noreply.github.com>
2022-09-21 15:55:01 +0200

    feat: create book button

---
+++ components/shrBook.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="container" class="bg-custom-black text-custom-pink rounded-full py-18 px-68 text-2xl hover:text-custom-purple hover:cursor-pointer hover:ring-2 hover: ring-inset hover:ring-custom-purple hover:drop-shadow-custom-shadow">
+        <p id="border-pink">{{content}}</p>
+    </div>
+</template>
+
+<style>
+#border-pink{
+    text-shadow: -0.5px 0 #FAE0E4, 0 0.5px #FAE0E4, 0.5px 0 #FAE0E4, 0 -0.5px #FAE0E4;
+}
+
+#container:hover #border-pink{
+    text-shadow: -0.5px 0 #A391E1, 0 0.5px #A391E1, 0.5px 0 #A391E1, 0 -0.5px #A391E1;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    content: {
+            type: String,
+            default: "default"
+        }
+    }
+}
+</script>
