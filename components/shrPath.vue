9c8d869dae264498d2858c87f7a5224dd861805a
Luca Stave <98452756+staveluca@users.noreply.github.com>
2022-09-21 15:55:43 +0200

    feat: create Path component for routes

---
+++ components/shrPath.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="text-center">
+        <p class="text-4xl">{{content}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+      props: {
+        content: {
+                type: String,
+                default: "default"
+            }
+        }
+    }
+</script>
